3cb81ee6da6fc2b5c267a0a90dce41d47f8f6dbf
outingting <outingting0526@gmail.com>
2021-12-12 00:07:46 -0500

    edited rosenbrock test

--- ES-Funcs-Experiments/test-rosenbrock.py
+++ ES-Funcs-Experiments/test-rosenbrock.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 from scipy.stats import multivariate_normal
-from methods import ES_Hessian, ES_vanilla_gradient, Hess_Aware, LP_Gradient, LP_Hessian, LP_Hessian_structured, LP_Hessian_structured_v2, LP_Hessian_structured_v3, LP_Hessian_structured_v4, LP_Hessian_structured_v6, LP_Hessian_structured_v5, LP_Hessian_structured_v7
+from methods import ES_Hessian, ES_vanilla_gradient, Hess_Aware, LP_Gradient, LP_Hessian, LP_Hessian_structured, LP_Hessian_structured_v2, LP_Hessian_structured_v3, LP_Hessian_structured_v4, LP_Hessian_structured_v6, LP_Hessian_structured_v5, LP_Hessian_structured_v7, asebo
 import cvxpy as cp
 import matplotlib.pyplot as plt 
 
@@ -26,10 +26,10 @@ initial_pt = np.random.uniform(-2,2,100)
 # res = ES_vanilla_gradient(F, alpha=1e-4, sigma=0.05, theta_0=initial_pt, num_samples = 20, time_steps = 200)
 # plt.plot(res[3], res[4], label = "ES_vanilla_gradient")
 
-res = ES_Hessian(F, alpha=0.5, sigma=0.05, theta_0=initial_pt, num_samples = 100, time_steps = 100*20)
+res = ES_Hessian(F, alpha=0.5, sigma=0.05, theta_0=initial_pt, num_samples = 25, time_steps = 100*20)
 plt.plot(res[3], res[4], label = "ES_Hessian")
 
-res = Hess_Aware(F, alpha = 0.1, sigma = 0.05, theta_0=initial_pt, num_samples = 100, time_steps = 67*20)
+res = Hess_Aware(F, alpha = 0.1, sigma = 0.05, theta_0=initial_pt, num_samples = 25, time_steps = 67*20)
 plt.plot(res[3], res[4], label = "Hess_Aware")
 #
 # res = LP_Hessian(F, alpha = 0.1, sigma = 0.05, theta_0=initial_pt, num_samples = 100, time_steps = 100)
@@ -45,20 +45,24 @@ plt.plot(res[3], res[4], label = "Hess_Aware")
 # plt.plot(res[3], res[4], label = "LP_Hessian_structured v3")
 
 print("LP Hessian structured with PT inverse, antithetic samples and backtracking")
-res = LP_Hessian_structured_v4(F, alpha = 1e-6, sigma = 0.01, theta_0=initial_pt, PT_threshold=-1e1, num_samples = 100, time_steps = 100)
+res = LP_Hessian_structured_v4(F, alpha = 1e-6, sigma = 0.01, theta_0=initial_pt, PT_threshold=-1e1, num_samples = 25, time_steps = 100*20)
 plt.plot(res[3], res[4], label = "LP_Hessian_structured_v4")
 
 print("LP Hessian structured with PT inverse, antithetic samples and backtracking [simulation gradient estimator]")
-res = LP_Hessian_structured_v5(F, alpha = 1e-6, sigma = 0.01, sigma_g =0.01, theta_0=initial_pt, PT_threshold=-1e1, num_samples = 100, time_steps = 100)
+res = LP_Hessian_structured_v5(F, alpha = 1e-6, sigma = 0.01, sigma_g =0.01, theta_0=initial_pt, PT_threshold=-1e1, num_samples = 25, time_steps = 100*20)
 plt.plot(res[3], res[4], label = "LP_Hessian_structured_v5")
 
 print("LP Hessian structured with PT inverse, antithetic samples and backtracking [linear regression structured hessian estimator]")
-res = LP_Hessian_structured_v6(F, alpha = 1e-6, sigma = 0.01, sigma_g =0.01, theta_0=initial_pt, PT_threshold=-1e1, num_samples = 100, time_steps = 100)
+res = LP_Hessian_structured_v6(F, alpha = 1e-6, sigma = 0.01, sigma_g =0.01, theta_0=initial_pt, PT_threshold=-1e1, num_samples = 25, time_steps = 100*20)
 plt.plot(res[3], res[4], label = "LP_Hessian_structured_v6")
 
 print("LP Hessian structured with PT inverse, antithetic samples and backtracking [unconstrained linear regression structured hessian estimator]")
-res = LP_Hessian_structured_v7(F, alpha = 1e-6, sigma = 0.01, sigma_g =0.01, theta_0=initial_pt, PT_threshold=-1e1, num_samples = 100, time_steps = 100)
+res = LP_Hessian_structured_v7(F, alpha = 1e-6, sigma = 0.01, sigma_g =0.01, theta_0=initial_pt, PT_threshold=-1e1, num_samples = 25, time_steps = 100*20)
 plt.plot(res[3], res[4], label = "LP_Hessian_structured_v7")
+
+print("Running ASEBO")
+res = asebo(F, sigma=0.05, learning_rate=0.05, decay=0.99, k=140, theta_0=initial_pt, min_samples=25, num_sensings=50, max_iter=1000, use_log=False, threshold=0.995)
+plt.plot(res[2], res[3], label = "asebo")
 
 plt.legend(loc="lower right")
 plt.xlabel("# function calls")
